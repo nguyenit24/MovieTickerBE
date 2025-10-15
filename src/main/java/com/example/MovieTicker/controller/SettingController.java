dfe9c2507e261b53bb53bd5a8658b2c138b356b1
Dev Huy <thaiquanghuy10a8.12@gmail.com>
2025-10-15 21:02:16 +0700

    Bổ sung CauHinhHeThong

--- src/main/java/com/example/MovieTicker/controller/SettingController.java
+++ src/main/java/com/example/MovieTicker/controller/SettingController.java
@@ -89,7 +89,5 @@ public class SettingController {
                     .build();
         }
     }
-
-
 }
 
--- src/main/java/com/example/MovieTicker/entity/CauHinhHeThong.java
+++ src/main/java/com/example/MovieTicker/entity/CauHinhHeThong.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table
 public class CauHinhHeThong implements Serializable {
 
     @Id
